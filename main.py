22183f1171a68b7a514d3cd9cd9f982d441e03e3
drossos <kostas.drosos@icloud.com>
2020-02-17 16:56:20 +0200

    Main entry point.

---
+++ main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+from processes import method
+from tools.file_io import load_yaml_file
+from tools.printing import init_loggers
+from tools.argument_parsing import get_argument_parser
+
+__author__ = 'Konstantinos Drossos -- Tampere University'
+__docformat__ = 'reStructuredText'
+__all__ = ['main']
+
+
+def main():
+    args = get_argument_parser().parse_args()
+
+    file_dir = args.file_dir
+    config_file = args.config_file
+    file_ext = args.file_ext
+    verbose = args.verbose
+
+    settings = load_yaml_file(Path(
+        file_dir, f'{config_file}.{file_ext}'))
+
+    init_loggers(verbose=verbose,
+                 settings=settings['logging'])
+
+    method.method(settings)
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
