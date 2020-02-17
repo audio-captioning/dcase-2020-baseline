e4d5fbf086f9d214b51cb5283f1831b9f5ee0cfd
drossos <kostas.drosos@icloud.com>
2020-02-17 17:04:24 +0200

    init of tools.

---
+++ tools/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from tools import argument_parsing
+from tools import file_io
+from tools import model
+from tools import printing
+
+__author__ = 'Konstantinos Drossos -- Tampere University'
+__docformat__ = 'reStructuredText'
+__all__ = ['argument_parsing', 'file_io', 'model', 'printing']
+
+# EOF
