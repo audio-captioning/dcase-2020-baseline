ebba1e00e017adcf1b598f10c4fd393c23c94b80
drossos <kostas.drosos@icloud.com>
2020-02-21 10:17:21 +0200

    Included captions functions.

--- tools/__init__.py
+++ tools/__init__.py
@@ -3,6 +3,7 @@
 
 from tools import argument_parsing
 from tools import dataset_creation
+from tools import captions_functions
 from tools import csv_functions
 from tools import file_io
 from tools import model
@@ -10,7 +11,8 @@ from tools import printing
 
 __author__ = 'Konstantinos Drossos -- Tampere University'
 __docformat__ = 'reStructuredText'
-__all__ = ['argument_parsing', 'dataset_creation', 'csv_functions',
+__all__ = ['argument_parsing', 'dataset_creation',
+           'captions_functions', 'csv_functions',
            'file_io', 'model', 'printing']
 
 # EOF
