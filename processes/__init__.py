7800ab218328e283e6f3377d8424423e563fa3c7
drossos <kostas.drosos@icloud.com>
2020-02-27 10:28:55 +0200

    Removed unused import.

--- processes/__init__.py
+++ processes/__init__.py
@@ -3,12 +3,10 @@
 
 from . import dataset
 from . import features
-from . import features_log_mel_bands
 from . import method
 
 __author__ = 'Konstantinos Drossos -- Tampere University'
 __docformat__ = 'reStructuredText'
-__all__ = ['dataset', 'features',
-           'features_log_mel_bands', 'method']
+__all__ = ['dataset', 'features', 'method']
 
 # EOF
