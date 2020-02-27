ce74fd48c1a5b7f53000e4a0ddfc0315a76177af
drossos <kostas.drosos@icloud.com>
2020-02-27 10:27:37 +0200

    Init file for the processes package.

---
+++ processes/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from . import dataset
+from . import features
+from . import features_log_mel_bands
+from . import method
+
+__author__ = 'Konstantinos Drossos -- Tampere University'
+__docformat__ = 'reStructuredText'
+__all__ = ['dataset', 'features',
+           'features_log_mel_bands', 'method']
+
+# EOF
