148e4d9ecb2cd18a0abcb594deebbaca001aa652
drossos <kostas.drosos@icloud.com>
2020-03-02 11:17:55 +0200

    Added logging and fixes.

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 
 from pathlib import Path
 
-from processes import method
+from loguru import logger
+
+import evaluation
+from processes import method, dataset
 from tools.file_io import load_yaml_file
 from tools.printing import init_loggers
 from tools.argument_parsing import get_argument_parser
@@ -25,9 +28,26 @@ def main():
         file_dir, f'{config_file}.{file_ext}'))
 
     init_loggers(verbose=verbose,
-                 settings=settings['logging'])
+                 settings=settings['dirs_and_files'])
 
-    method.method(settings)
+    logger_main = logger.bind(is_caption=False, indent=1)
+
+    if settings['workflow']['dataset_creation']:
+        logger_main.info('Starting creation of dataset')
+        dataset.create_dataset(
+            settings_dataset=settings['dataset_creation_settings'],
+            settings_dirs_and_files=settings['dirs_and_files'])
+        logger_main.info('Creation of dataset ended')
+
+    if settings['workflow']['dnn_training']:
+        logger_main.info('Starting optimization of method')
+        method.method(settings)
+        logger_main.info('Optimization of method ended')
+
+    if settings['workflow']['captions_evaluation']:
+        logger_main.info('Starting evaluation of captions')
+        evaluation.evaluate_captions(settings)
+        logger_main.info('Evaluation of captions ended')
 
 
 if __name__ == '__main__':
