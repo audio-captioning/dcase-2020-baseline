e05543da527e27c337971df8cfeed53a537d3c95
drossos <kostas.drosos@icloud.com>
2020-03-03 14:25:59 +0200

    Added feature extraction import.

--- tools/__init__.py
+++ tools/__init__.py
@@ -8,11 +8,13 @@ from tools import csv_functions
 from tools import file_io
 from tools import model
 from tools import printing
+from tools import features_log_mel_bands
 
 __author__ = 'Konstantinos Drossos -- Tampere University'
 __docformat__ = 'reStructuredText'
 __all__ = ['argument_parsing', 'dataset_creation',
            'captions_functions', 'csv_functions',
-           'file_io', 'model', 'printing']
+           'features_log_mel_bands', 'file_io',
+           'model', 'printing']
 
 # EOF
