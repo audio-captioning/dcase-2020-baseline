307ae66ed853efd7b45a50592b3e4831461c43fb
drossos <kostas.drosos@icloud.com>
2020-03-03 11:10:35 +0200

    Added function for feature extraction.

--- processes/dataset.py
+++ processes/dataset.py
@@ -5,7 +5,9 @@ from typing import MutableMapping, Any
 from datetime import datetime
 from pathlib import Path
 from functools import partial
+from itertools import chain
 
+import numpy as np
 from loguru import logger
 
 from tools.printing import init_loggers
@@ -13,11 +15,13 @@ from tools.argument_parsing import get_argument_parser
 from tools.dataset_creation import get_annotations_files, \
     get_amount_of_file_in_dir, check_data_for_split, \
     create_split_data, create_lists_and_frequencies
-from tools.file_io import load_settings_file, load_yaml_file
+from tools.file_io import load_settings_file, load_yaml_file, \
+    load_numpy_object, dump_numpy_object
+from tools.features_log_mel_bands import feature_extraction
 
 __author__ = 'Konstantinos Drossos -- Tampere University'
 __docformat__ = 'reStructuredText'
-__all__ = ['create_dataset']
+__all__ = ['create_dataset', 'extract_features']
 
 
 def create_dataset(settings_dataset: MutableMapping[str, Any],
@@ -137,6 +141,94 @@ def create_dataset(settings_dataset: MutableMapping[str, Any],
             inner_logger.info(f'Skipping validation of {split_name} split')
 
 
+def extract_features(root_dir: str,
+                     settings_data: MutableMapping[str, Any],
+                     settings_features: MutableMapping[str, Any])\
+        -> None:
+    """Extracts features from the audio data of Clotho.
+
+    :param root_dir: Root dir for the data.
+    :type root_dir: str
+    :param settings_data: Settings for creating data files.
+    :type settings_data: dict[str, T]
+    :param settings_features: Settings for feature extraction.
+    :type settings_features: dict[str, T]
+    """
+    # Get the root directory.
+    dir_root = Path(root_dir)
+
+    # Get the directories of files.
+    dir_output = dir_root.joinpath(settings_data['audio_dirs']['output'])
+
+    dir_dev = dir_output.joinpath(
+        settings_data['audio_dirs']['development'])
+    dir_eva = dir_output.joinpath(
+        settings_data['audio_dirs']['evaluation'])
+
+    # Get the directories for output.
+    dir_output_dev = dir_root.joinpath(
+        settings_data['features_dirs']['output'],
+        settings_data['features_dirs']['development'])
+    dir_output_eva = dir_root.joinpath(
+        settings_data['features_dirs']['output'],
+        settings_data['features_dirs']['evaluation'])
+
+    # Create the directories.
+    dir_output_dev.mkdir(parents=True, exist_ok=True)
+    dir_output_eva.mkdir(parents=True, exist_ok=True)
+
+    # Apply the function to each file and save the result.
+    for data_file_name in filter(
+            lambda _x: _x.suffix == '.npy',
+            chain(dir_dev.iterdir(), dir_eva.iterdir())):
+
+        # Load the data file.
+        data_file = load_numpy_object(data_file_name)
+
+        # Extract the features.
+        features = feature_extraction(
+            data_file['audio_data'].item(),
+            **settings_features['process'])
+
+        # Populate the recarray data and dtypes.
+        array_data = (data_file['file_name'].item(), )
+        dtypes = [('file_name', data_file['file_name'].dtype)]
+
+        # Check if we keeping the raw audio data.
+        if settings_features['keep_raw_audio_data']:
+            # And add them to the recarray data and dtypes.
+            array_data += (data_file['audio_data'].item(), )
+            dtypes.append(('audio_data', data_file['audio_data'].dtype))
+
+        # Add the rest to the recarray.
+        array_data += (
+            features,
+            data_file['caption'].item(),
+            data_file['caption_ind'].item(),
+            data_file['words_ind'].item(),
+            data_file['chars_ind'].item())
+        dtypes.extend([
+            ('features', np.dtype(object)),
+            ('caption', data_file['caption'].dtype),
+            ('caption_ind', data_file['caption_ind'].dtype),
+            ('words_ind', data_file['words_ind'].dtype),
+            ('chars_ind', data_file['chars_ind'].dtype)
+        ])
+
+        # Make the recarray
+        np_rec_array = np.rec.array([array_data], dtype=dtypes)
+
+        # Make the path for serializing the recarray.
+        parent_path = dir_output_dev \
+            if data_file_name.parent.name == settings_data['audio_dirs']['development'] \
+            else dir_output_eva
+
+        file_path = parent_path.joinpath(data_file_name.name)
+
+        # Dump it.
+        dump_numpy_object(np_rec_array, file_path)
+
+
 def main():
 
     args = get_argument_parser().parse_args()
@@ -153,6 +245,7 @@ def main():
                  settings=settings['dirs_and_files']['logging'])
 
     logger_main = logger.bind(is_caption=False, indent=1)
+    logger_sec = logger.bind(is_caption=False, indent=2)
 
     logger_main.info(datetime.now().strftime('%Y-%m-%d %H:%M'))
 
@@ -165,9 +258,20 @@ def main():
 
     # Create the dataset.
     logger_main.info('Starting Clotho dataset creation')
+
+    logger_sec.info('Creating examples')
     create_dataset(
         settings_dataset=settings['dataset_creation_settings'],
         settings_dirs_and_files=settings['dirs_and_files'])
+    logger_sec.info('Examples created')
+
+    logger_sec.info('Extracting features')
+    extract_features(
+        root_dir=settings['dirs_and_files']['root_dirs']['data'],
+        settings_data=settings['dirs_and_files']['dataset'],
+        settings_features=settings['feature_extraction_settings'])
+    logger_sec.info('Features extracted')
+
     logger_main.info('Dataset created')
 
 
